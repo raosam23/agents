23195b2627b56e4e5ccb5f5e7febe98611c0d912
Samarth Rao <raosamarth1234@gmail.com>
2025-12-04 01:41:03 +0530

    week2 day5

--- 2_openai/deep_research/email_agent.py
+++ 2_openai/deep_research/email_agent.py
@@ -5,27 +5,23 @@ import sendgrid
 from sendgrid.helpers.mail import Email, Mail, Content, To
 from agents import Agent, function_tool
 
-
 @function_tool
 def send_email(subject: str, html_body: str) -> Dict[str, str]:
-    """Send an email with the given subject and HTML body"""
-    sg = sendgrid.SendGridAPIClient(api_key=os.environ.get("SENDGRID_API_KEY"))
-    from_email = Email("ed@edwarddonner.com")  # put your verified sender here
-    to_email = To("ed.donner@gmail.com")  # put your recipient here
-    content = Content("text/html", html_body)
+    ''' Send an email with the given subject and HTML body '''
+    sg = sendgrid.SendGridAPIClient(api_key = os.environ.get('SENDGRID_API_KEY'))
+    from_email = Email('raosamarth1234@gmail.com')
+    to_email = To('bapnadsamarth@gmail.com')
+    content = Content('text/html', html_body)
     mail = Mail(from_email, to_email, subject, content).get()
-    response = sg.client.mail.send.post(request_body=mail)
-    print("Email response", response.status_code)
-    return "success"
+    response = sg.mail.send.post(request_body = mail)
+    print('Email response', response.status_code)
+    return {'status': 'success'}
 
-
-INSTRUCTIONS = """You are able to send a nicely formatted HTML email based on a detailed report.
-You will be provided with a detailed report. You should use your tool to send one email, providing the 
-report converted into clean, well presented HTML with an appropriate subject line."""
+INSTRUCTIONS = f'You are able to send a nicely formatted HTML email based on a detailed report. You will be provided with a detailed report. You should use your tool to send one email, providing the report converted into a clean and well presented HTML with an appropriate subject line.'
 
 email_agent = Agent(
-    name="Email agent",
-    instructions=INSTRUCTIONS,
-    tools=[send_email],
-    model="gpt-4o-mini",
+    name = 'Email agent',
+    instructions = INSTRUCTIONS,
+    tools = [send_email],
+    model = 'gpt-4o-mini',
 )
