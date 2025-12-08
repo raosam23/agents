c1eb580f4bc49c98e361cadae3292717b7865eab
Samarth Rao <raosamarth1234@gmail.com>
2025-12-09 00:30:15 +0530

    week3 day 3

--- 3_crew/stock_picker/src/stock_picker/tools/push_tool.py
+++ 3_crew/stock_picker/src/stock_picker/tools/push_tool.py
@@ -5,25 +5,24 @@ import os
 import requests
 
 
-class PushNotification(BaseModel):
-    """A message to be sent to the user"""
-    message: str = Field(..., description="The message to be sent to the user.")
+class PushNotificationInput(BaseModel):
+    ''' Message to be sent to the user '''
+    message: str = Field(..., description='The message to be sent to the user')
 
 class PushNotificationTool(BaseTool):
-    
-
-    name: str = "Send a Push Notification"
+    name: str = 'Send a push notification'
     description: str = (
-        "This tool is used to send a push notification to the user."
+        'This tool is used to send push notification to the user.'
     )
-    args_schema: Type[BaseModel] = PushNotification
+    args_schema: Type[BaseModel] = PushNotificationInput
 
-    def _run(self, message: str) -> str:
-        pushover_user = os.getenv("PUSHOVER_USER")
-        pushover_token = os.getenv("PUSHOVER_TOKEN")
-        pushover_url = "https://api.pushover.net/1/messages.json"
+    def _run(self, message:str) -> str:
+        # Implementation goes here
+        pushover_user = os.getenv('PUSHOVER_USER')
+        pushover_token = os.getenv('PUSHOVER_TOKEN')
+        pushover_url = 'https://api/pushover.net/1/messages.json'
 
-        print(f"Push: {message}")
-        payload = {"user": pushover_user, "token": pushover_token, "message": message}
+        print(f'Push : {message}')
+        payload = {'user': pushover_user, 'token': pushover_token, 'message': message}
         requests.post(pushover_url, data=payload)
         return '{"notification": "ok"}'
